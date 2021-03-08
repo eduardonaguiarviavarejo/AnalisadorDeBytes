9fe9c45681a7e8761921a016b2afa2f7ddb9ce80
Eduardo Nascimento Aguiar <eduardo.aguiar-ext@viavarejo.com.br>
2021-03-08 20:13:39 -0300

    Testes e Correçoes

--- AnalisadorDeBytes/AnalisadorDeBytes.Testes/Dominio/AnalisadorDeBytesTeste.cs
+++ AnalisadorDeBytes/AnalisadorDeBytes.Testes/Dominio/AnalisadorDeBytesTeste.cs
@@ -1,7 +1,64 @@
-﻿namespace AnalisadorDeBytes.Testes.Dominio
+﻿using AnalisadorDeBytes.Dominio;
+using AnalisadorDeBytes.Dominio.Comandos;
+using AnalisadorDeBytes.Dominio.Respostas;
+using AnalisadorDeBytes.IoC;
+using Moq;
+using System;
+using Xunit;
+
+namespace AnalisadorDeBytes.Testes.Dominio
 {
     public class AnalisadorDeBytesTeste
     {
+        private readonly string _caminhoFisicoArquivo = @"c:/dev";
+        private readonly int _tamanhoMaximoBufferEmMegaBytes = 1;
+        private readonly string _texto = "O fluxo deve se repetir até que o arquivo tenha o tamanho de 100MB como tamanho padrão";
+        private readonly Analisador _analisador;
+        private readonly Mock<IBuscarTextoEmSite> _buscarTextoEmSite = new Mock<IBuscarTextoEmSite>();
 
+        public AnalisadorDeBytesTeste()
+        {
+            _analisador = new Analisador(_buscarTextoEmSite.Object);
+        }
+
+        [Fact]
+        public async void ProcessarAsync_DeveriaGerarMetricas()
+        {
+            _buscarTextoEmSite.Setup(x => x.ExecutarAsync(new BuscarTextoEmSiteComandos(new Uri("http://www.lerolero.com.br"))))
+                .ReturnsAsync(new BuscarTextoEmSiteResposta(_texto));
+
+        }
+
+
+
+        [Fact]
+        public async void AnalyseAsync_DeveriaGerarMetricasComTamanhoDoBufferDiferente()
+        {
+            var infos = await _analisador.ProcessarAsync(_caminhoFisicoArquivo, 5);
+        }
+
+
+
+        [Fact]
+        public void AnalyseAsync_DeveriaGerarMetricasFallbackCrawl()
+        {
+
+        }
+
+
+
+        [Fact]
+        public void AnalyseAsync_DeveriaGerarMetricasFallbackContadorDeBytes()
+        {
+
+        }
+
+
+
+        [Fact]
+        public void AnalyseAsync_NaoDeveriaGerarMetricasSemCaminhoArquivo()
+        {
+
+        }
     }
 }
